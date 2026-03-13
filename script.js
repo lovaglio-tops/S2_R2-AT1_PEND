e667adb75bfe923538154c803718f4237429b9a5
DANIEL ALMEIDA LOVAGLIO <daniel.lovaglio2@senaisp.edu.br>
2026-03-13 13:03:33 -0300

    ex3 corrigido

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-let nomeCliente = document.getElementById("nomeCliente")
-let btnSuccess = document.getElementById("btnSuccess")
+let inputTarefa = document.getElementById("inputTarefa")
+let btnAdicionar = document.getElementById("btnAdicionar")
 let mensagem = document.getElementById("mensagem")
-let nomeArmazenado;
 
+btnAdicionar.addEventListener("click", () => {
 
-btnSuccess.addEventListener("click", () => {
-nomeArmazenado = nomeCliente.value
+    let nomeArmazenado = inputTarefa.value
+
+    if (nomeArmazenado === "") {
+        mensagem.innerText = 'Digite algo!'
+        mensagem.className = 'text-danger fw-bold'
+    } else {
+
+        mensagem.innerText = 'Tarefa inserida'
+        mensagem.className = 'text-success fw-bold'
+
+        let li = document.createElement("li")
+        
    
-if (nomeArmazenado==="") {
-    mensagem.innerText = 'insira um nome' 
-}else{
-    mensagem.innerText = `hello, ${nomeArmazenado}`
-}   
+        
 
+        li.innerText = nomeArmazenado
 
+        
+        
+
+        let lista = document.getElementById("listaTarefas")
+        lista.appendChild(li)
+        
+        
+
+        
+    }
 })
