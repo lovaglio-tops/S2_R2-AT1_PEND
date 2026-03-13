64799dec114974a53c2343b739e6dd7e7f1a1982
DANIEL ALMEIDA LOVAGLIO <daniel.lovaglio2@senaisp.edu.br>
2026-03-13 12:52:51 -0300

    ex1

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-let texto = document.getElementById("texto")
+let nomeCliente = document.getElementById("nomeCliente")
 let btnSuccess = document.getElementById("btnSuccess")
-
+let mensagem = document.getElementById("mensagem")
+let nomeArmazenado;
 
 
 btnSuccess.addEventListener("click", () => {
+nomeArmazenado = nomeCliente.value
+   
+if (nomeArmazenado==="") {
+    mensagem.innerText = 'insira um nome' 
+}else{
+    mensagem.innerText = `hello, ${nomeArmazenado}`
+}   
 
-   texto.innerText = `Eu mandei não apertar,👺💣 kbumm`
-    
 
 })
