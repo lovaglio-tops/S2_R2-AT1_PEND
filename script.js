8d90c1736a27e2fd4fb2768852a875f14ffca5ae
DANIEL ALMEIDA LOVAGLIO <daniel.lovaglio2@senaisp.edu.br>
2026-03-13 13:13:47 -0300

    ex1

--- script.js
+++ script.js
@@ -1,49 +1,18 @@
-let inputTarefa = document.getElementById("inputTarefa")
-let btnAdicionar = document.getElementById("btnAdicionar")
+let nomeCliente = document.getElementById("nomeCliente")
+let btnSuccess = document.getElementById("btnSuccess")
 let mensagem = document.getElementById("mensagem")
+let nomeArmazenado;
 
-btnAdicionar.addEventListener("click", () => {
 
-    let nomeArmazenado = inputTarefa.value
 
-    if (nomeArmazenado === "") {
-        mensagem.innerText = 'Digite algo!'
-        mensagem.className = 'text-danger fw-bold'
-    } else {
+btnSuccess.addEventListener("click", () => {
+nomeArmazenado = nomeCliente.value
 
-        mensagem.innerText = 'Tarefa inserida'
-        mensagem.className = 'text-success fw-bold'
+ if (nomeArmazenado==="") {
+    mensagem.innerText = 'insira um nome'  
+ }else{   
+ mensagem.innerText = `hello,${nomeArmazenado}`
+ }  
 
-        let li = document.createElement("li")
-        let buttonLi = document.createElement("button")
-        let checkbox = document.createElement("input")
-        checkbox.type = 'checkbox'
 
-        buttonLi.textContent = "Remover"
-
-        li.innerText = nomeArmazenado
-
-        
-        li.appendChild(checkbox)
-        li.appendChild(buttonLi)
-
-        let lista = document.getElementById("listaTarefas")
-        lista.appendChild(li)
-        
-        checkbox.addEventListener("change", () => {
-            if (checkbox.checked) {
-                li.style.color = "green"
-            } else {
-                li.style.color = "black"
-            }
-        })
-
-        
-        buttonLi.addEventListener("click", () => {
-            li.remove()
-            mensagem.innerText=""
-        })
-
-        inputTarefa.value = ""
-    }
 })
